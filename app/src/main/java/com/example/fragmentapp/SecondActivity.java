cb7bc9224a256542a3ef94c7f8d3d6fe9df7ae23
trihariyadi <trihariyadi24@gmail.com>
2021-04-18 19:35:46 +0700

    initial commit

---
+++ app/src/main/java/com/example/fragmentapp/SecondActivity.java
@@ -0,0 +1,24 @@
+package com.example.fragmentapp;
+
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SecondActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.v("CREATING", "SECOND ACTIVITY");
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            finish();
+            return;
+        }
+
+        if (savedInstanceState == null) {
+            SecondFragment details = new SecondFragment();
+            getFragmentManager().beginTransaction().add(android.R.id.content, details).commit();
+        }
+    }
+}
